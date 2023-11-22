5288b4cea5e47c2d398966e5e62e9a6c10870c25
preciousken <pkehinde27@gmail.com>
2023-11-22 11:12:14 +0100

    first commit

--- configuration.js
+++ configuration.js
@@ -4,7 +4,7 @@ const configData = {
     FLUTTER_BASE_API_URL: process.env.FLUTTER_BASE_API_URL,
     FLUTTER_SECRET_KEY:process.env.FLUTTER_SECRET_KEY,
     CURRENCIES:[
-        "NG","GH","KE","UG","ZA","TZ","US","UK"
+        "AED", "ARS", "AUD", "CAD", "CHF", "CZK", "ETB", "EUR", "GBP", "GHS", "ILS", "INR", "JPY", "KES", "MAD", "MUR", "MYR", "NGN", "NOK", "NZD", "PEN", "PLN", "RUB", "RWF", "SAR", "SEK", "SGD", "SLL", "TZS", "UGX", "USD", "XAF", "XOF", "ZAR", "ZMK", "ZMW", "MWK"
     ]
 }
 
