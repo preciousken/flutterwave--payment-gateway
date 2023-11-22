e555ed993829bde472b5e89aa8d91ce4fc77817f
preciousken <pkehinde27@gmail.com>
2023-11-22 15:23:05 +0100

    updated

--- bankTransfer.js
+++ bankTransfer.js
@@ -4,19 +4,29 @@ const Flutterwave = require('flutterwave-node-v3');
 const flw = new Flutterwave(configData.FLUTTER_PUBLIC_KEY, configData.FLUTTER_SECRET_KEY);
 
 const bankTransfer = async() => {
+
+    SENDING_CURRENCY = "TRY";
+    RECEIVING_CURRENCY = "NGN"
     
-const details = {
-    account_bank: "044",
-    account_number: "0690000040",
-    amount: 200,
-    narration: "Payment for things",
-    currency: "NGN",
-    // reference: generateTransactionReference(),
-    callback_url: "https://webhook.site/b3e505b0-fe02-430e-a538-22bbbce8ce0d",
-    debit_currency: "NGN"
-};
-await flw.Transfer.initiate(details)
-    .then(console.log)
-    .catch(console.log);
+    // Check if the sender currency can transact with the receiving currency
+    if(!configData.CURRENCY_MATCH[SENDING_CURRENCY].includes(RECEIVING_CURRENCY)){
+        console.log('you can\'t send money to this currency')
+        return
+    }else{
+        
+    const details = {
+        account_bank: "044",
+        account_number: "0690000040",
+        amount: 200,
+        narration: "Payment for things",
+        currency: RECEIVING_CURRENCY,
+        // reference: generateTransactionReference(),
+        callback_url: "https://webhook.site/b3e505b0-fe02-430e-a538-22bbbce8ce0d",
+        debit_currency: SENDING_CURRENCY
+    };
+    await flw.Transfer.initiate(details)
+        .then(console.log)
+        .catch(console.log);
+    }
 }
 bankTransfer()
