97c60794a11a269b41460207047bdca7cbd7512e
preciousken <pkehinde27@gmail.com>
2023-11-24 19:27:57 +0100

    updated

---
+++ createVirtualAccount.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const { configData } = require('./configuration');
+
+const createVirtualCard = async() => {
+
+    const data = {
+        "email":"user@gmail.com",
+        "is_permanent":true,
+        "bvn":"12345678901",
+        "tx_ref":"Sample_tx_ref",
+        "phonenumber":"08100000000",
+        "firstname":"Flutterwave",
+        "lastname":"Developers",
+        "narration":"Add_the_name_of_account_here"
+     }
+     
+
+     try {
+        const response = await axios.post(
+            `${configData.FLUTTER_BASE_API_URL}/virtual-account-numbers`,
+            data,
+            {
+                headers: {
+                    Authorization: `Bearer ${configData.FLUTTER_SECRET_KEY}`,
+                },
+            }
+        );
+
+        console.log(response.data);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+createVirtualCard()
